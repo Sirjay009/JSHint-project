9e41b3607a154f1ea7cb44c0fa4701e062e9c224
Chima Obutte <obuttechima@rocketmail.com>
2024-11-20 02:09:04 +0000

    Create and modify style.css, index.html and script.js files

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const API_KEY = "yGs_XCGTjeASXpfwpzRdbUFycJk";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
