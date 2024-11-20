176be58199c65410acfb633a9d1ae0f5c79022df
Chima Obutte <obuttechima@rocketmail.com>
2024-11-20 03:30:55 +0000

    Write the postForm function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,23 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+    const response = await fetch(API_URL, {
+        method: "POST",
+        header: {"Authorization": API_KEY,},
+        body: form,
+    });
+    const data = await response.json();
+    if(response.ok) {
+        console.log(data);
+    } else {
+        throw new Error(data.error);
+    }
+
+};
 
 function displayStatus(data) {
     let heading = "API Key Status";
